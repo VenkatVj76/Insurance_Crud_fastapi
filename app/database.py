56cfa0de412e6669a70ed94b3b5f306e72bb762b
venkat <venkat@Venkateshwarans-MacBook-Air.local>
2025-10-22 18:24:06 +0530

    MONGO_URL fix

--- app/database.py
+++ app/database.py
@@ -4,7 +4,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
+MONGO_URL = os.getenv("MONGO_URL")
 DB_NAME = os.getenv("DB_NAME", "test")
 
 client = AsyncIOMotorClient(MONGO_URL)
